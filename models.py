a2b7ed3da540f0118029401dc6baf3fb0a41bf9a
Lilyanne Tisdale <lilyanne.tisdale@gmail.com>
2022-11-10 11:40:22 -0600

    created models.py to create database(?)

---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Person %r>' % self.username
+
+
+
+
+# class Person(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+
+#     def __repr__(self) ->str:
+#         return f'Person with username: {self.username} and email {self.email}'
+
+    
+# with app.app_context():
+#     db.create_all()
