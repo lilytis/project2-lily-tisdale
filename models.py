a3ef0c4b5ac8c21bdd2bad498f9bad1b7db16b28
Lilyanne Tisdale <lilyanne.tisdale@gmail.com>
2022-11-11 08:31:57 -0600

    added Review class to be implemented

--- models.py
+++ models.py
@@ -6,5 +6,11 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
 
-    def __repr__(self):
-        return f"Person with username: {self.username}"
+    # def __repr__(self):
+    #     return f"Person with username: {self.username}"
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating_score = db.Column(db.Integer)
+    review = db.Column(db.String(500))
+    
