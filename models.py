16611025e67a8fc6e8646666aa70af5041cac883
Lilyanne Tisdale <lilyanne.tisdale@gmail.com>
2022-11-11 08:16:36 -0600

    clean up

--- models.py
+++ models.py
@@ -7,19 +7,4 @@ class Person(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Person %r>' % self.username
-
-
-
-
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-
-#     def __repr__(self) ->str:
-#         return f'Person with username: {self.username} and email {self.email}'
-
-    
-# with app.app_context():
-#     db.create_all()
+        return f"Person with username: {self.username}"
