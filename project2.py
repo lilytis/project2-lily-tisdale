eea61eb5ddeaf162818e6ce98b65742b3de5262a
Lilyanne Tisdale <lilyanne.tisdale@gmail.com>
2022-11-12 15:38:31 -0600

    Log-in/Register working, users are saving to database
    major changes (oops) --

--- project2.py
+++ project2.py
@@ -1,14 +1,12 @@
 import os
 import flask
 from flask_sqlalchemy import SQLAlchemy
-# import models
-# from models import Person
-# from models import Review
 from urllib import response
 import requests
 import json
 from dotenv import load_dotenv, find_dotenv
 from random import randrange
+import flask_login
 
 load_dotenv(find_dotenv())
 
@@ -39,9 +37,24 @@ class Review(db.Model):
 with app.app_context():
     db.create_all()
 
+def get_wiki_url(title):
+    request=requests.Session()
+    WIKI_URL="https://en.wikipedia.org/w/api.php"
+    params={
+        "action":"opensearch",
+        "namespace":"0",
+        "search": str(title), #+ "movie"
+        "limit":"1",
+        "format":"json"
+    }
+    wiki_info=request.get(url=WIKI_URL, params=params)
+    get_info=wiki_info.json()
+    return get_info[3][0]
+
 def get_movies():    
     MOVIE_IDS=[642885, 616820, 760161]
-    MOVIE_PATH=f'/movie/{MOVIE_IDS[randrange(3)]}'
+    CHOSEN_MOVIE=f'{MOVIE_IDS[randrange(3)]}'
+    MOVIE_PATH=f'/movie/{CHOSEN_MOVIE}'
     MOVIE_API_BASE_URL=f'https://api.themoviedb.org/3{MOVIE_PATH}'
     IMAGE_URL='https://image.tmdb.org/t/p/w500'
 
@@ -58,27 +71,16 @@ def get_movies():
         genres_of_movie += str(genre['name'])+", "
     wiki_url=get_wiki_url(title = this_movie['original_title'])
     movie_info=[this_movie['original_title'], this_movie['tagline'],
-                    genres_of_movie, image_path, wiki_url]
+                genres_of_movie, image_path, wiki_url, CHOSEN_MOVIE]
 
     return movie_info
-
-def get_wiki_url(title):
-    request=requests.Session()
-    WIKI_URL="https://en.wikipedia.org/w/api.php"
-    params={
-        "action":"opensearch",
-        "namespace":"0",
-        "search": str(title), #+ "movie"
-        "limit":"1",
-        "format":"json"
-    }
-    wiki_info=request.get(url=WIKI_URL, params=params)
-    get_info=wiki_info.json()
-    return get_info[3][0]
 
 @app.route('/')
 def index():
     return flask.render_template('home.html')
+def hello():
+    people = Person.query.all()
+    return repr(people)
 # def index():
 #     return [str(person) for person in Person.query.all()]
 
@@ -95,14 +97,19 @@ def register():
 @app.route('/register', methods=['POST'])
 def register_post():
     form_data=flask.request.form
-    Person.username = form_data['username']
-    if flask.request.method=='POST' and 'username' in flask.request.form:
-        return flask.flash('User already exists. Please log in.')
-    else:
-        person=Person(username=Person.username)
-        db.session.add(person)
-        db.session.commit()
-        return f'Created person with username: {Person.username}'
+    Person.username = form_data.get('username')
+    person=Person.query.filter_by(username=Person.username).first()
+
+    if person:
+        flask.flash('User already exists. Please log in.')
+        return flask.redirect(flask.url_for('register'))
+
+    new_person = Person(username=Person.username)
+    db.session.add(new_person)
+    db.session.commit()
+
+    return flask.redirect(flask.url_for('login'))
+
 
 @app.route('/login')
 def login():
@@ -111,22 +118,21 @@ def login():
 @app.route('/login', methods=['POST'])
 def login_post():
     form_data=flask.request.form
-    print(f'FORM DATA: {form_data}')
-    Person.username=form_data['username']
-    if flask.request.method=='POST' and 'username' in flask.request.form:
-        Person.username=flask.request.form['username']
-        flask.flash('Logged in successfully!')
-        return flask.redirect(flask.url_for('movies'))
-    else:
-        return flask.flash('Try again. Username invalid.')
-        #return flask.redirect(flask.url_for('login'))
+    Person.username=form_data.get('username')
+    person=Person.query.filter_by(username=Person.username).first()
+
+    if not person:
+        flask.flash('User does not exist. Please try again or create account.')
+        return flask.redirect(flask.url_for('login'))
+
+    return flask.redirect(flask.url_for('movies'))
 
 @app.route('/movies')
 def movies():
     list_of_movie_info = get_movies()
     return flask.render_template('movies.html', title=list_of_movie_info[0], 
         tagline=list_of_movie_info[1], genres=list_of_movie_info[2], 
-        image=list_of_movie_info[3], link=list_of_movie_info[4])
+        image=list_of_movie_info[3], link=list_of_movie_info[4], id=list_of_movie_info[5])
 
 @app.route('/movies', methods=['POST'])
 def movies_post():
@@ -137,4 +143,4 @@ def movies_post():
     Review.review=form_data['review']
     return flask.flash('Thank you for submitting your review!')
     
-#app.run()
+#app.run(debug=True)
